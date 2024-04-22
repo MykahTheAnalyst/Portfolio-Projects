098e0c065ae32a489fcbbb9c9439695558cd46b9
MykahTheAnalyst <166565789+MykahTheAnalyst@users.noreply.github.com>
2024-04-22 16:56:03 -0500

    SQL Data Cleaning Query1.sql

--- SQL/SQL Data Cleaning Query1.sql
+++ SQL/SQL Data Cleaning Query1.sql
@@ -1,4 +1,5 @@
-
+#1. Detect and remove duplicates.
+	
 WITH cte1 as (
 Select CustomerID, ShipName, ShipAddress, ShipPostalCode, ShipCountry,
 ROW_NUMBER() OVER(partition by CustomerID order by CustomerID) as rn
@@ -7,6 +8,7 @@ From Orders
 
 select * from cte1 where rn  = 1
 
+#2. Clean data with CASE Statments.
 
 Select freight,
 case when freight between 0 and 50 then 'low charge'
@@ -16,7 +18,9 @@ case when freight between 0 and 50 then 'low charge'
 from Orders
 
 
-
+	
+#3. Find NULL Values and replace with No Region.
+	
 select CustomerID, contactName, city, 
 ISNULL (Region, 'No Region') as region
 from customers
