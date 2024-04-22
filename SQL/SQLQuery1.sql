41bd7e77a37ddbca12e937ce1043b4d263741568
MykahTheAnalyst <166565789+MykahTheAnalyst@users.noreply.github.com>
2024-04-22 14:57:40 -0500

    Add files via upload

---
+++ SQL/SQLQuery1.sql
@@ -0,0 +1,22 @@
+
+WITH cte1 as (
+Select CustomerID, ShipName, ShipAddress, ShipPostalCode, ShipCountry,
+ROW_NUMBER() OVER(partition by CustomerID order by CustomerID) as rn
+From Orders
+)
+
+select * from cte1 where rn  = 1
+
+
+Select freight,
+case when freight between 0 and 50 then 'low charge'
+	WHEN  Freight between 50 and 200 then 'medium charge' 
+	WHEN freight > 200 then 'High Charge'
+	end as Charge
+from Orders
+
+
+
+select CustomerID, contactName, city, 
+ISNULL (Region, 'No Region') as region
+from customers
